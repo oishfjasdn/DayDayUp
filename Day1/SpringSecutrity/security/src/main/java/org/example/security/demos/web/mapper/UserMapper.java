d52f0a1b66f0bb9dd804158a52acfe252abccce2
ncc-1701 <142652856+oishfjasdn@users.noreply.github.com>
2024-10-28 23:10:56 +0800

    Delete Day1/SpringSecutrity/security directory

--- Day1/SpringSecutrity/security/src/main/java/org/example/security/demos/web/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package org.example.security.demos.web.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.example.security.demos.web.domain.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
